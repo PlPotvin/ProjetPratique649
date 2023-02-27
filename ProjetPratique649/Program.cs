790d0014b908022481a1fb124b590adb21f6dc53
Administrator <Administrator@PCUPPY>
2023-02-27 17:06:12 -0500

    Add project files.

---
+++ ProjetPratique649/Program.cs
@@ -0,0 +1,15 @@
+﻿// Variables instanciations
+int[] numbers = new int[50];
+int nbr = 0;
+Random ran = new Random();
+
+nbr = ran.Next(1, 49);
+
+for(int i = 1; i < numbers.Length; i++)
+    {
+    numbers[i] = i;
+    Console.Write(numbers[i] + " ");
+    }
+
+
+Console.WriteLine(nbr);
