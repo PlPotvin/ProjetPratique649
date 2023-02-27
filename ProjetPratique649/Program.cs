d1821148502ff613ca24df125ea59e4abdd4268b
PlPotvin <potvinpierreluc@gmail.com>
2023-02-27 17:56:08 -0500

    Added a user input for six numbers and then display and compare both sequences. No verification for the numbers is done.

--- ProjetPratique649/Program.cs
+++ ProjetPratique649/Program.cs
@@ -1,9 +1,28 @@
 ﻿// Variables instanciations
 int[] winNbr = new int[6];
+int[] chosenNbr = new int[6];
 Random ran = new Random();
 
+// Asking for user numbers and making sure they are right.
+
+for (int i = 0; i < chosenNbr.Length; i++)
+{
+    Console.WriteLine("Please pick your numbers " + (i + 1) + "/6");
+    chosenNbr[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("Your numbers are ");
+for (int i =0; i < chosenNbr.Length; i++ )
+{
+    Console.Write(chosenNbr[i] + " ");
+}
+Console.WriteLine();
+
+// Displaying winning numbers
+
+Console.WriteLine("The winning numbers are : ");
 for(int i = 0; i < winNbr.Length; i++)
-    {
-        winNbr[i] = ran.Next(1,49);
-        Console.Write(winNbr[i] + " ");
-    }
+{
+    winNbr[i] = ran.Next(1,49);
+    Console.Write(winNbr[i] + " ");
+}
